118df5b82b4506ff781adcb6e722ef9e4839d652
lia <k2amj2ik@gmail.com>
2014-08-05 18:24:51 +0900

    angular test

--- angular/js/app.js
+++ angular/js/app.js
@@ -1,5 +1,49 @@
 app = angular.module("myApp", []);
 
-app.controller("mainCtrl", function($scope)) {
+
+//app.controller("mainCtrl", ["$scope", "personService", function($scope, personService){}]);
+
+app.controller("mainCtrl", function($scope) {
   $scope.name = "Default Name";
+  $scope.people = [];
+  $scope.savePerson = function() {
+    $scope.people.push[$scope.name];
+    alert($scope.name);
+    $scope.name = '';
+  };
 });
+
+
+app.directive("alertable", function () {
+  return {
+    restrict: "A",
+    link: function(scope, element, attrs) {
+      element.bind("click", function() {
+        alert(attrs.message);
+      });
+    }
+  };
+});
+
+
+app.factory("personService", function() {
+  var personService = {};
+  personService.people = [];
+  personService.addPerson = function(person) {
+    personService.people.push(person);
+  };
+  return personService;
+});
+
+
+
+//app.config(function ($routeProvider) {
+//  $routeProvider.when("/", {
+//    templateUrl: "templates/home.html",
+//    controller: "homepageCtrl"
+//  });
+//  $routeProvider.when("/person/:id", {
+//    templateUrl: "templates/profile.html",
+//    controller: "profileCtrl"
+//  });
+//});
