4fb967d05c3af63250ef3ef0f9fe269032ce8698
k2amj2ik <k2amj2ik@gmail.com>
2019-06-21 18:40:15 +0900

    홈피 제이크 수정본

---
+++ bi-homepage/js/script.js
@@ -0,0 +1,9 @@
+function b() {
+    $(".menus").removeClass("hide");
+    $(".menus").addClass("active");
+}
+function a(){
+    console.log("asdfasdfasdf");
+    $(".menus").removeClass("active");
+    $(".menus").addClass("hide");
+}
