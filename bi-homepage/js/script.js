dddf6962563230aadc2e69c732caf73c432a6f99
k2amj2ik <k2amj2ik@gmail.com>
2019-06-25 14:41:12 +0900

    홈피 웹/모바일

--- bi-homepage/js/script.js
+++ bi-homepage/js/script.js
@@ -6,3 +6,22 @@ function menusHide(){
     $(".menus").removeClass("active");
     $(".menus").addClass("hide");
 }
+$(document).ready(function() {
+    console.log("ready")
+    $(".sp-monopoly").click(function(){
+        $(".sub-container .submenu .title").parent().removeClass("active");
+        $(".sp-monopoly").parent().addClass("active");
+    });
+    $(".sp-contributor").click(function(){
+        $(".sub-container .submenu .title").parent().removeClass("active");
+        $(".sp-contributor").parent().addClass("active");
+    });
+    $(".sp-validator").click(function(){
+        $(".sub-container .submenu .title").parent().removeClass("active");
+        $(".sp-validator").parent().addClass("active");
+    });
+    $(".sp-datasharing").click(function(){
+        $(".sub-container .submenu .title").parent().removeClass("active");
+        $(".sp-datasharing").parent().addClass("active");
+    });
+});
